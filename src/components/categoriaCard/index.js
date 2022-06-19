9a8fe227b63ea914c1f61d94533cc7db959dde1e
Pedro Teles <ppedrocteles@gmail.com>
2022-06-19 19:53:27 -0300

    responsivity

--- src/components/categoriaCard/index.js
+++ src/components/categoriaCard/index.js
@@ -12,9 +12,9 @@ const useStyles = makeStyles({
     },
     root2: {
         color: "#01873B",
-        fontWeight:"bold",
+        fontWeight: "bold",
         fontFamily: "'Montserrat', sans-serif;",
-        fontSize: "clamp(15px,2vw,80px)",
+        fontSize: "clamp(20px,2vw,80px)",
         marginTop: "2rem"
     },
     p: {
@@ -31,20 +31,15 @@ const useStyles = makeStyles({
         height: 48,
         width: "50vw",
         padding: '0 0',
-        margin:"0.5rem 0",
+        margin: "0.5rem 0",
     },
-    buttonText:{
+    buttonText: {
         color: "#002443",
         fontFamily: "'Montserrat', sans-serif",
         fontSize: "1rem",
-        fontWeight:"bold"
+        fontWeight: "bold"
     },
     card: {
-        background: "#fff",
-        padding:"1vw",
-        margin:"0.5vw 0.5vw",
-        height:"clamp(15px,10vh,100px)",
-        width:"clamp(50px,28vw,300px)",
         boxShadow: '0 -0.3rem 0 0.05rem rgba(1, 135, 59, 1)'
     }
 })
@@ -67,25 +62,25 @@ const CategoryCard = () => {
     return (
         <>
             <Stack>
-                <Container sx={{ marginTop:"1rem",marginBottom: "5rem", display: "flex", flexDirection: "column", alignItens: "center", justifyContent: "center" }}>
-                    <h1>QUEM PODE PARTICIPAR?</h1>
-                    <p className={classes.p}>A olimpiada da independência sera dividida em tres categorias</p>
+                <Container sx={{ marginTop: "1rem", marginBottom: "5rem", display: "flex", flexDirection: "column", alignItens: "center", justifyContent: "center" }}>
+                    <h1>QUEM PODE PARTICIPAR?<br/><br/></h1>
+                    <p className={classes.p}>A olimpiada da independência sera dividida em três categorias</p>
                 </Container>
-                <Stack direction="row" className={responsive.Stack}>
-                    <Card className={classes.card} sx={{ minHeight: "20rem", maxWidth: "33vw", margin: "1rem", backgrouColor: "#F2F2F2", padding: "1rem 2rem", borderRadius: "1rem" }}>
+                <div className={responsive.Stack}>
+                    <Card className={responsive.Card} sx={{ minHeight: "20rem", maxWidth: "33vw", margin: "1rem", backgrouColor: "#F2F2F2", padding: "1rem 2rem", borderRadius: "1rem" }}>
                         <h3 className={classes.root} >{categorias[0].nome}</h3>
                         <p className={classes.root2} sx={{ marginTop: "6rem", marginBottom: "2rem", marginX: "2rem 0rem", width: "20rem" }}>{categorias[0].texto}</p>
                     </Card>
-                    <Card className={classes.card} sx={{ minHeight: "20rem", maxWidth: "33vw", margin: "1rem", backgrouColor: "#F2F2F2", padding: "1rem 2rem", borderRadius: "1rem" }}>
+                    <Card className={responsive.Card} sx={{ minHeight: "20rem", maxWidth: "33vw", margin: "1rem", backgrouColor: "#F2F2F2", padding: "1rem 2rem", borderRadius: "1rem" }}>
                         <h3 className={classes.root} >{categorias[1].nome}</h3>
                         <p className={classes.root2} sx={{ marginTop: "6rem", marginBottom: "2rem", marginX: "2rem 0rem", width: "20rem" }}>{categorias[1].texto}</p>
                     </Card>
-                    <Card className={classes.card} sx={{ minHeight: "20rem", maxWidth: "33vw", margin: "1rem", backgrouColor: "#F2F2F2", padding: "1rem 2rem", borderRadius: "1rem" }}>
+                    <Card className={responsive.Card} sx={{ minHeight: "20rem", maxWidth: "33vw", margin: "1rem", backgrouColor: "#F2F2F2", padding: "1rem 2rem", borderRadius: "1rem" }}>
                         <h3 className={classes.root} >{categorias[2].nome}</h3>
                         <p className={classes.root2} sx={{ marginTop: "6rem", marginBottom: "2rem", marginX: "2rem 0rem", width: "20rem" }}>{categorias[2].texto}</p>
                     </Card>
-                </Stack>
-                <Container sx={{marginBottom:"4rem"}}>
+                </div>
+                <Container sx={{ marginBottom: "4rem" }}>
                     <Button className={classes.button}><p className={classes.buttonText}>INSCREVA-SE</p></Button>
                 </Container>
             </Stack>
