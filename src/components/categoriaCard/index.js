f3ff0dab607e7c1ccd1bd9ed9cc8a4b58e0e0b90
Pedro Teles <ppedrocteles@gmail.com>
2022-06-19 16:05:08 -0300

    color changes + responsivity

--- src/components/categoriaCard/index.js
+++ src/components/categoriaCard/index.js
@@ -6,36 +6,46 @@ import { fontFamily } from '@mui/system';
 import responsive from './categoriacard.module.css'
 const useStyles = makeStyles({
     root: {
-        color: "blue",
+        color: "#002443",
         fontFamily: "'Montserrat', sans-serif",
-        fontSize: "3rem",
-        width: "20rem"
+        fontSize: "clamp(5px,8vw,50px)",
     },
     root2: {
-        color: "green",
+        color: "#01873B",
+        fontWeight:"bold",
         fontFamily: "'Montserrat', sans-serif;",
-        fontSize: "1.5rem",
-        width: "20rem",
-        marginTop: "4rem"
+        fontSize: "clamp(15px,2vw,80px)",
+        marginTop: "2rem"
     },
     p: {
         color: "yellow",
         fontFamily: "'Montserrat', sans-serif",
         fontSize: "2rem",
-        paddingTop:"5rem"
     },
     button: {
-        background: 'linear-gradient(45deg, #F2F2F2 30%, #B2B2B2 90%)',
+        background: 'linear-gradient(45deg, #FFF 30%, #FFF 90%)',
         border: 0,
         borderRadius: 3,
         boxShadow: '0 0.15rem 0.02rem 0.05rem rgba(255, 255, 255, .3)',
         color: 'black',
         height: 48,
         width: "50vw",
-        padding: '0 0'
+        padding: '0 0',
+        margin:"0.5rem 0",
+    },
+    buttonText:{
+        color: "#002443",
+        fontFamily: "'Montserrat', sans-serif",
+        fontSize: "1rem",
+        fontWeight:"bold"
     },
     card: {
-        background: 'linear-gradient(45deg, #F2F2F2 30%, #B2B2B2 90%)'
+        background: "#fff",
+        padding:"1vw",
+        margin:"0.5vw 0.5vw",
+        height:"clamp(15px,10vh,100px)",
+        width:"clamp(50px,28vw,300px)",
+        boxShadow: '0 -0.3rem 0 0.05rem rgba(1, 135, 59, 1)'
     }
 })
 const CategoryCard = () => {
@@ -57,7 +67,7 @@ const CategoryCard = () => {
     return (
         <>
             <Stack>
-                <Container sx={{ marginBottom: "5rem", display: "flex", flexDirection: "column", alignItens: "center", justifyContent: "center" }}>
+                <Container sx={{ marginTop:"1rem",marginBottom: "5rem", display: "flex", flexDirection: "column", alignItens: "center", justifyContent: "center" }}>
                     <h1>QUEM PODE PARTICIPAR?</h1>
                     <p className={classes.p}>A olimpiada da independência sera dividida em tres categorias</p>
                 </Container>
@@ -75,8 +85,8 @@ const CategoryCard = () => {
                         <p className={classes.root2} sx={{ marginTop: "6rem", marginBottom: "2rem", marginX: "2rem 0rem", width: "20rem" }}>{categorias[2].texto}</p>
                     </Card>
                 </Stack>
-                <Container>
-                    <Button className={classes.button}><p>INSCREVA-SE</p></Button>
+                <Container sx={{marginBottom:"4rem"}}>
+                    <Button className={classes.button}><p className={classes.buttonText}>INSCREVA-SE</p></Button>
                 </Container>
             </Stack>
         </>
