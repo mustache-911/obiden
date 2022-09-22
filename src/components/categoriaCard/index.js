814b35e54c7928a86325dc46b617ba341e4e76b1
pedro <ppedrocteles@gmail.com>
2022-09-22 00:09:47 -0300

    fixed responsivity : categoryCard

--- src/components/categoriaCard/index.js
+++ src/components/categoriaCard/index.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import { useState } from 'react';
-import { makeStyles } from '@material-ui/styles';
-import { Typography, Stack, Card, CardContent, Container, Button } from '@mui/material';
-import { fontFamily } from '@mui/system';
+import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import {
+    Stack,
+    Card,
+    Container,
+} from '@mui/material'
 import responsive from './categoriacard.module.css'
 const useStyles = makeStyles({
     root: {
-        color: "#002443",
+        color: '#002443',
         fontFamily: "'Montserrat', sans-serif",
-        fontSize: "clamp(5px,4vw,50px)",
+        fontSize: 'clamp(25px,4vw,50px)'
     },
     root2: {
-        color: "#212121",
-        fontWeight: "-moz-initial",
+        color: '#212121',
+        fontWeight: '-moz-initial',
         fontFamily: "'Montserrat', sans-serif;",
-        fontSize: "clamp(20px,1.2vw,80px)",
-        marginTop: "2rem"
+        fontSize: 'clamp(13px,3.5vw,15px)',
+        marginTop: '2rem'
     },
     p: {
-        color: "#FCB805",
+        color: '#FCB805',
         fontFamily: "'Montserrat', sans-serif",
-        fontSize: "2rem",
+        fontSize: '2rem'
     },
     // button: {
     //     background: 'linear-gradient(45deg, #FFF 30%, #FFF 90%)',
@@ -38,10 +40,10 @@ const useStyles = makeStyles({
     //     opacity:"0.6"
     // },
     buttonText: {
-        color: "#002443",
+        color: '#002443',
         fontFamily: "'Montserrat', sans-serif",
-        fontSize: "1.2rem",
-        fontWeight: "bold"
+        fontSize: '1.2rem',
+        fontWeight: 'bold'
     },
     card: {
         boxShadow: '0 -0.3rem 0 0.05rem rgba(1, 135, 59, 1)'
@@ -51,44 +53,94 @@ const CategoryCard = () => {
     const classes = useStyles()
     const categorias = [
         {
-            nome: "Júnior",
-            texto: "Categoria destinada aos alunos do 8o e 9o anos do Ensino Fundamental"
-        },
+            nome: 'Júnior',
+            texto: 'Categoria destinada a estudantes nascidos depois de 1º de janeiro de 2008, inclusive. Salientamos que as questões serão elaboradas para estudantes acima de 11 anos. Entretanto está facultado o direito, aos estudantes mais novos, de participar desta categoria.'
+        },   
         {
-            nome: "Livre",
-            texto: "Categoria destinada a qualquer pessoa,sendo independente de idade e formação academica"
-        },
-        {
-            nome: "Sênior",
-            texto: "Categoria destinada aos alunos do ensino médio"
+            nome: 'Sênior',
+            texto: 'Categoria destinada a estudantes nascidos entre de 31 de dezembro de 2007, inclusive, e 1o de janeiro de 2004, inclusive, desde que não estejam matriculados em algum curso superior.'
         }
     ]
     return (
         <>
             <Stack>
-                <Container sx={{ marginTop: "1rem", marginBottom: "5rem", display: "flex", flexDirection: "column", alignItens: "center", justifyContent: "center" }}>
-                    <h1>QUEM PODE PARTICIPAR?<br /><br /></h1>
-                    <p className={classes.p}>A Olimpíada da Independência será dividida em três categorias</p>
+                <Container
+                    sx={{
+                        marginTop: '1rem',
+                        marginBottom: '5rem',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItens: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <h1>
+                        QUEM PODE PARTICIPAR?
+                        <br />
+                        <br />
+                    </h1>
+                    <p className={classes.p}>
+                        A Olimpíada da Independência será dividida em duas
+                        categorias
+                    </p>
                 </Container>
                 <div className={responsive.Stack}>
-                    <Card className={`${responsive.Card}`} sx={{ minHeight: "20rem", maxWidth: "33vw", margin: "1rem", backgrouColor: "#F2F2F2", padding: "1rem 2rem", borderRadius: "1rem" }}>
-                        <h3 className={classes.root} >{categorias[0].nome}</h3>
-                        <p className={classes.root2} sx={{ marginTop: "6rem", marginBottom: "2rem", marginX: "2rem 0rem", width: "20rem" }}>{categorias[0].texto}</p>
+                    <Card
+                        className={`${responsive.Card}`}
+                        sx={{
+                            minHeight: 'clamp(80px,33vw,320px)',
+                            maxWidth: '90vw',
+                            margin: '1rem',
+                            backgrouColor: '#F2F2F2',
+                            padding: '1rem 2rem',
+                            borderRadius: '1rem'
+                        }}
+                    >
+                        <h3 className={classes.root}>{categorias[0].nome}</h3>
+                        <p
+                            className={classes.root2}
+                            sx={{
+                                marginTop: '6rem',
+                                marginBottom: '2rem',
+                                marginX: '2rem 0rem',
+                                width: '25rem',
+                            }}
+                        >
+                            {categorias[0].texto}
+                        </p>
                     </Card>
-                    <Card className={responsive.Card} sx={{ minHeight: "20rem", maxWidth: "33vw", margin: "1rem", backgrouColor: "#F2F2F2", padding: "1rem 2rem", borderRadius: "1rem" }}>
-                        <h3 className={classes.root} >{categorias[1].nome}</h3>
-                        <p className={classes.root2} sx={{ marginTop: "6rem", marginBottom: "2rem", marginX: "2rem 0rem", width: "20rem" }}>{categorias[1].texto}</p>
+                    <Card
+                        className={responsive.Card}
+                        sx={{
+                            minHeight: 'clamp(80px,33vw,320px)',
+                            maxWidth: '90vw',
+                            margin: '1rem',
+                            backgrouColor: '#F2F2F2',
+                            padding: '1rem 2rem',
+                            borderRadius: '1rem'
+                        }}
+                    >
+                        <h3 className={classes.root}>{categorias[1].nome}</h3>
+                        <p
+                            className={classes.root2}
+                            sx={{
+                                marginTop: '6rem',
+                                marginBottom: '2rem',
+                                marginX: '2rem 0rem',
+                                width: '20rem',
+                                wordWrap: 'break-word'
+                            }}
+                        >
+                            {categorias[1].texto}
+                        </p>
                     </Card>
-                    <Card className={responsive.Card} sx={{ minHeight: "20rem", maxWidth: "33vw", margin: "1rem", backgrouColor: "#F2F2F2", padding: "1rem 2rem", borderRadius: "1rem" }}>
-                        <h3 className={classes.root} >{categorias[2].nome}</h3>
-                        <p className={classes.root2} sx={{ marginTop: "6rem", marginBottom: "2rem", marginX: "2rem 0rem", width: "20rem" }}>{categorias[2].texto}</p>
-                    </Card>
+               
                 </div>
                 {/* <Container sx={{ marginBottom: "4rem" }}>
                     <Button className={responsive.Button}><p className={classes.buttonText}>INSCREVA-SE</p></Button>
                 </Container> */}
             </Stack>
         </>
-    );
+    )
 }
-export default CategoryCard;
+export default CategoryCard
