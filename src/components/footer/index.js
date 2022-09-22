d35d02cfa141c78ec0f96cacf118bd484817166c
pedro <ppedrocteles@gmail.com>
2022-09-21 22:56:29 -0300

    responsivity fix: categoryCard

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -187,7 +187,7 @@ const ResponsiveToolBar = () => {
                                 )
                             }
                         />
-
+!
                         <Box
                             component="img"
                             sx={{
